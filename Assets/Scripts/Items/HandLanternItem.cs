026f9f9d5ec81c6cb9001b7a1b8915c3d445b6fa
Moisés Carpena Caicedo <mcarcai@upv.edu.es>
2022-11-09 18:30:47 +0100

    actualizacion

--- Assets/Scripts/Items/HandLanternItem.cs
+++ Assets/Scripts/Items/HandLanternItem.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class HandLanternItem : HandItem
 {
     private Light lanternLight;
-    public Light luzLinterna;
-    public bool activeLight; 
     public virtual void Awake()
     {
         lanternLight = GetComponentInChildren<Light>();
@@ -17,20 +15,6 @@ public class HandLanternItem : HandItem
         if (lanternLight)
         {
             lanternLight.enabled = !lanternLight.enabled;
-        }
-    }
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            activeLight = !activeLight;
-
-            if(activeLight == true)
-                luzLinterna.enabled = true;
-
-            if(activeLight == false)
-                luzLinterna.enabled = false;
         }
     }
 }
